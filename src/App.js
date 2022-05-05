61c0f12b5f3a0420d8065c5dcae6db2340741ec0
Jonke-ec <jonas.askling@utb.ecutbildning.se>
2022-05-05 10:56:03 +0200

    Navbar one created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import NavbarOne from "./components/shared/NavbarOne";
 import { NavbarThree } from "./components/shared/NavbarThree";
 
 function App() {
   return (
     <>
+      <NavbarOne />
       <NavbarThree />
     </>
   );
---
+++ src/components/shared/NavbarOne.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const NavbarOne = () => {
+    return (
+      <>
+          <div class="container d-flex">
+              <div class="group">
+                <span>ENG</span>
+                <span>USD</span>
+              </div>
+              <div class="group">
+                <span>Newsletter</span>
+                <span>Contact us</span>
+                <span></span>
+                <span></span>
+              </div>
+          </div>
+      </>
+    );
+  };
+  
+  export default NavbarOne;
