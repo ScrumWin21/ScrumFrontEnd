9803929abba57661559fa246c993719949c6a9b9
Tylerstratford <tylerstratford@hotmail.com>
2022-05-06 09:03:37 +0200

    showcase 1 added

---
+++ src/components/homeShowcaseOne/homeShowcaseOne.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const HomeShowcaseOne = () => {
+  return (
+    <>
+      <div id="home-showcase-one">
+        <div className="ContainerRegular flex">
+          <div id="home-showcaseOne-left">
+            <div>
+              <h3>Shopping is</h3>
+              <h2>MORE FUN</h2>
+              <p>
+                Vivamus dignissim a ex eu ornare. Ut fermentum orci vel diam
+                ultricies faucibus. Quisque quis erat sed
+              </p>
+              <button id="home-showcaseOne-button">SHOP NOW</button>
+            </div>
+          </div>
+          <div id="home-showcaseTwo-right">
+            <h3>picture</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
