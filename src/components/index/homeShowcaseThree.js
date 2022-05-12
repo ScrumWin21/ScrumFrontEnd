8e32c36c1c2d7f8584733747bc7bb9ceb62d4131
Tylerstratford <tylerstratford@hotmail.com>
2022-05-12 10:27:46 +0200

    showcase three grid completed

--- src/components/index/homeShowcaseThree.js
+++ src/components/index/homeShowcaseThree.js
@@ -16,7 +16,70 @@ export const HomeShowcaseThree = () => {
             reliable way
           </p>
         </div>
-        <div id="homeShowcaseThree-grid"></div>
+        <div className="ContainerRegular margin-lr-auto">
+          <div id="homeShowcaseThree-grid">
+            {/* box One */}
+            <div className="home-grid-box-gray">
+              <div className="home-grid-box-blue">
+                <h3>Men</h3>
+              </div>
+            </div>
+            {/* box Two */}
+            <div
+              className="home-grid-box-gray"
+              id="home-showcaseThree-grid-boxTwo"
+            >
+              <div className="home-grid-box-blue">
+                <h3>Women</h3>
+              </div>
+            </div>
+            {/* box Three */}
+            <div
+              className="home-grid-box-gray"
+              id="home-showcaseThree-grid-boxThree"
+            >
+              <div className="home-grid-box-blue">
+                <h3>Kids</h3>
+              </div>
+            </div>
+            {/* box four */}
+            <div
+              className="home-grid-box-gray"
+              id="home-showcaseThree-grid-boxFour"
+            >
+              <div className="home-grid-box-blue">
+                <h3>Shoes</h3>
+              </div>
+            </div>
+            {/* box five */}
+            <div
+              className="home-grid-box-gray"
+              id="home-showcaseThree-grid-boxFive"
+            >
+              <div className="home-grid-box-blue">
+                <h3>Hats</h3>
+              </div>
+            </div>
+            {/* box six */}
+            <div
+              className="home-grid-box-gray"
+              id="home-showcaseThree-grid-boxSix"
+            >
+              <div className="home-grid-box-blueTwo">
+                <div id="box-blueTwo-large">
+                  <h3>Sunglasses</h3>
+                  <h4>18 Products</h4>
+                </div>
+              </div>
+            </div>
+            {/* box seven */}
+            <div className="home-grid-box-gray">
+              <div className="home-grid-box-blue">
+                <h3>Watches</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
