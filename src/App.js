f225bf7948908cfd0594ff70943f2801e0d4f704
Aleksandar Todorovic <90002225+aaleksandart@users.noreply.github.com>
2022-05-17 14:07:42 +0200

    Adding structure.
    adding folder for pages that also include the complete navbar that imports all 3 parts of navbar components.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import "./App.scss";
-import { HomeShowcaseTwo } from "./components/index/homeShowcaseTwo";
-import { HomeShowcaseOne } from "./components/index/homeShowcaseOne";
-import NavbarOne from "./components/shared/NavbarOne";
-import { NavbarThree } from "./components/shared/NavbarThree";
-import { HomeShowcaseThree } from "./components/index/homeShowcaseThree";
-import { HomeShowcaseFour } from "./components/index/homeShowcaseFour";
-import HomeShowcaseFive from "./components/index/homeShowcaseFive";
-import HomeShowcaseSeven from "./components/index/homeShowcaseSeven";
-import HomeShowcaseEight from "./components/index/homeShowcaseEight";
 
-import HomeShowcaseEleven from "./components/index/homeShowcaseEleven";
-import HomeShowcaseTwelve from "./components/index/homeShowcaseTwelve";
+//SHARED
+import Navbar from "./pages/shared/navbar";
+
+//PAGES
+import Home from "./pages/home";
+
 
 function App() {
   return (
     <>
-      <NavbarOne />
-      <NavbarThree />
-      <HomeShowcaseOne />
-      <HomeShowcaseTwo />
-      <HomeShowcaseThree />
-      <HomeShowcaseFour />
-      <HomeShowcaseFive />
-      <HomeShowcaseSeven />
-      <HomeShowcaseEight />
-      <HomeShowcaseEleven />
-      <HomeShowcaseTwelve />
+
+      <Navbar />
+      <Home />
+
     </>
   );
 }
