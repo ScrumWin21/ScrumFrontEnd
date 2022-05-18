5c3272bf39da5cf38dacbbd1c83bd90c45a94258
Tylerstratford <tylerstratford@hotmail.com>
2022-05-18 14:12:44 +0200

    Item #83 Add text, Item #86 Add button

--- src/components/shopDetails/ShopComponents/ShopTopBanner.js
+++ src/components/shopDetails/ShopComponents/ShopTopBanner.js
@@ -3,7 +3,19 @@ import React from "react";
 export const ShopTopBanner = () => {
   return (
     <>
-      <div className="containerWide shopTopBannerContainer">hello world</div>
+      <div className="ContainerWide shopTopBannerContainer">
+        <div className="ContainerRegular flex margin-lr-auto shopTopBanner">
+          <div className="shopTopBannerLeft">
+            <h4>New Customer</h4>
+            <div className="shopTopBannerLeftText">
+              <h2>Can Get</h2>
+              <h3>30% Off</h3>
+            </div>
+            <button className="shopTopBannerButton">LEARN MORE</button>
+          </div>
+          <div className="shopTopBannerRight">Right</div>
+        </div>
+      </div>
     </>
   );
 };
--- src/views/ShopPage.js
+++ src/views/ShopPage.js
@@ -2,8 +2,9 @@ import React from "react";
 import { ShopTopBanner } from "../components/shopDetails/ShopComponents/ShopTopBanner";
 
 export const ShopPage = () => {
-    return <>
-        <ShopTopBanner />
-        <div>hello</div>
+  return (
+    <>
+      <ShopTopBanner />
     </>
+  );
 };
