7895b106e7ab4b510dc58f3ffe6b464ea64bb1a3
Tylerstratford <tylerstratford@hotmail.com>
2022-05-19 20:38:35 +0200

    Item #97 hard coding data completed, Item #98 loop through, display information correctly completed

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftBanner.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftBanner.js
@@ -1,6 +1,64 @@
 import React from "react";
 
+let productCategories = [
+  {
+    category: "Men's",
+    count: "18",
+  },
+  {
+    category: "Women's",
+    count: "23",
+    },
+    {
+        category: "Kids",
+        count: "32",
+    },
+    {
+        category: "Hats",
+        count: "12",
+    },
+    {
+        category: "Sunglasses",
+        count: "15",
+    },
+    {
+        category: "Shoes",
+        count: "12",
+    },
+    {
+        category: "Watches",
+        count: "20",
+      },
+];
+
 export const ShopBannerLeft = () => {
-    return <>
-    <div>Hello world!</div></>;
+  return (
+    <>
+      <div className="shopBannerLeft-title">Product Categories</div>
+      <div className="shopBannerLeft-container">
+        {productCategories.map((productCategories) => (
+          <div className="shopBannerLeft-container-content">
+            <div>{productCategories.category}</div>
+            <div className="shoperBannerLeft-numberBox">
+              {productCategories.count}
+            </div>
+          </div>
+        ))}
+
+        {/* <div className="shopBannerLeft-container-content">
+                        <div>{productCategories.product}</div>
+                        <div className="shoperBannerLeft-numberBox">{productCategories.count}</div>
+            </div> */}
+
+        {/* <div className="shopBannerLeft-container-content">
+                <div>Men's</div>
+                <div className="shoperBannerLeft-numberBox">18</div>
+            </div> */}
+        {/* <div className="shopBannerLeft-container-content">
+                <div>Men's</div>
+                <div className="shoperBannerLeft-numberBox">18</div>
+            </div> */}
+      </div>
+    </>
+  );
 };
