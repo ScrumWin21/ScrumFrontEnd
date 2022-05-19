222e89eb228722ea9d0c5aa972baa86f595df413
Tylerstratford <tylerstratford@hotmail.com>
2022-05-19 11:59:57 +0200

    Item #93 add shop text

--- src/components/shopDetails/ShopComponents/ShopFilter.js
+++ src/components/shopDetails/ShopComponents/ShopFilter.js
@@ -3,7 +3,17 @@ import React from "react";
 export const ShopFilter = () => {
   return (
     <>
-      <div className="shopPageFilterBackground"></div>
+      <div className="shopPageFilterBackground">
+        <div className="ContainerRegular margin-lr-auto">
+          <div className="shopPageFilter-left">
+            <h2 className="shopPageFilter-left-shop">Shop</h2>
+            <div>
+              <span>Home / </span>
+              <span className="shopPageFiler-left-shop-little">Shop</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
