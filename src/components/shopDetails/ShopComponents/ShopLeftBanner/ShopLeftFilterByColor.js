1ca804c50f0e853088eb33ae8ea85ce81937dd2f
Tylerstratford <tylerstratford@hotmail.com>
2022-05-20 12:59:59 +0200

    Item #102 Looping through and displaying data/colors correctly

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByColor.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByColor.js
@@ -1,69 +1,60 @@
 import React from "react";
 
-
-let shopColor = [{
-    color: "black",
+let shopColor = [
+  {
+    color: "#000000",
     text: "Black",
-    count: 12
-},
-    {
-        color: "gray",
+    count: 12,
+  },
+  {
+    color: "#EEEEEE",
     text: "Gray",
-    count: 20
-    },
-    {
-        color: "red",
+    count: 20,
+  },
+  {
+    color: "#FF2D2D",
     text: "Red",
-    count: 18
-    },
-    {
-        color: "green",
+    count: 18,
+  },
+  {
+    color: "#64CB83",
     text: "Green",
-    count: 25
-    },
-    {
-        color: "orange",
+    count: 25,
+  },
+  {
+    color: "#FFAA00",
     text: "Orange",
-    count: 9
-    },
-    {
-        color: "brown",
+    count: 9,
+  },
+  {
+    color: "#542323",
     text: "Brown",
-    count: 7
-    },
-]
+    count: 7,
+  },
+];
+
 
 export const ShopLeftFilterByColor = () => {
   return (
     <>
       <div className="shopBannerLeft-title">Filter by Color</div>
-          <div className="shopBannerLeft-container">
-              {shopColor.map((shopColor) => (
-                <div className="shopBannerLeft-container-content-color">
-                <div className="flex">
-                            <div className="shopBannerLeft-colorBox">
-                            <i class="fa-regular fa-check"></i>
-                  </div>
-                          <div>{shopColor.color }</div>
-                </div>
-      
-                      <div className="shopBannerLeft-numberBox">{ shopColor.count}</div>
+      <div className="shopBannerLeft-container">
+        {shopColor.map((shopColor) => (
+          <div className="shopBannerLeft-container-content-color">
+            <div className="flex">
+              <div
+                className="shopBannerLeft-colorBox"
+                style={{ backgroundColor: shopColor.color }}
+              >
+                <i class="fa-regular fa-check"></i>
               </div>
-              ))}
+              <div>{shopColor.text}</div>
             </div>
 
-                    
-        {/* <div className="shopBannerLeft-container-content-color">
-          <div className="flex">
-                      <div className="shopBannerLeft-colorBox">
-                      <i class="fa-regular fa-check"></i>
-            </div>
-            <div>Black</div>
+            <div className="shopBannerLeft-numberBox">{shopColor.count}</div>
           </div>
-
-          <div className="shopBannerLeft-numberBox">12</div>
-        </div>
-      </div> */}
+        ))}
+      </div>
     </>
   );
 };
