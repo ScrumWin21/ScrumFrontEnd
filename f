3eb4fcde596348b4f2e076259611a100f5f8b2f3
Tylerstratford <tylerstratford@hotmail.com>
2022-05-20 08:20:09 +0200

    Item #99 create and design filter by price completed

---
+++ f
@@ -0,0 +1,12 @@
+  addingSass[m
+  developer[m
+  feature-banner[m
+  feature-newPage[m
+  homeShowcaseFour[m
+  homeShowcaseOne[m
+  homeShowcaseThree[m
+  homeShowcaseTwo[m
+  master[m
+  shopPage[m
+* [32mshopPage-Feature-leftBanner[m
+  shopPage-feature-filter[m
--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftBanner.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftBanner.js
@@ -1,64 +1,13 @@
 import React from "react";
+import { ShopBannerLeftProductCategories } from "./ShopLeftBannerProductCategories";
+import { ShopFilterByPrice } from "./ShopLeftFilterByPrice";
 
-let productCategories = [
-  {
-    category: "Men's",
-    count: "18",
-  },
-  {
-    category: "Women's",
-    count: "23",
-    },
-    {
-        category: "Kids",
-        count: "32",
-    },
-    {
-        category: "Hats",
-        count: "12",
-    },
-    {
-        category: "Sunglasses",
-        count: "15",
-    },
-    {
-        category: "Shoes",
-        count: "12",
-    },
-    {
-        category: "Watches",
-        count: "20",
-      },
-];
 
 export const ShopBannerLeft = () => {
   return (
     <>
-      <div className="shopBannerLeft-title">Product Categories</div>
-      <div className="shopBannerLeft-container">
-        {productCategories.map((productCategories) => (
-          <div className="shopBannerLeft-container-content">
-            <div>{productCategories.category}</div>
-            <div className="shoperBannerLeft-numberBox">
-              {productCategories.count}
-            </div>
-          </div>
-        ))}
-
-        {/* <div className="shopBannerLeft-container-content">
-                        <div>{productCategories.product}</div>
-                        <div className="shoperBannerLeft-numberBox">{productCategories.count}</div>
-            </div> */}
-
-        {/* <div className="shopBannerLeft-container-content">
-                <div>Men's</div>
-                <div className="shoperBannerLeft-numberBox">18</div>
-            </div> */}
-        {/* <div className="shopBannerLeft-container-content">
-                <div>Men's</div>
-                <div className="shoperBannerLeft-numberBox">18</div>
-            </div> */}
-      </div>
+      <ShopBannerLeftProductCategories />
+      <ShopFilterByPrice />
     </>
   );
 };
