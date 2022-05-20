fa2f85e480b016fe95b624a36161ddded36ce37d
Tylerstratford <tylerstratford@hotmail.com>
2022-05-20 09:52:06 +0200

    Item #101 hard code data completed

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByColor.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByColor.js
@@ -1,11 +1,59 @@
 import React from "react";
 
+
+let shopColor = [{
+    color: "black",
+    text: "Black",
+    count: 12
+},
+    {
+        color: "gray",
+    text: "Gray",
+    count: 20
+    },
+    {
+        color: "red",
+    text: "Red",
+    count: 18
+    },
+    {
+        color: "green",
+    text: "Green",
+    count: 25
+    },
+    {
+        color: "orange",
+    text: "Orange",
+    count: 9
+    },
+    {
+        color: "brown",
+    text: "Brown",
+    count: 7
+    },
+]
+
 export const ShopLeftFilterByColor = () => {
   return (
     <>
       <div className="shopBannerLeft-title">Filter by Color</div>
-      <div className="shopBannerLeft-container">
-        <div className="shopBannerLeft-container-content-color">
+          <div className="shopBannerLeft-container">
+              {shopColor.map((shopColor) => (
+                <div className="shopBannerLeft-container-content-color">
+                <div className="flex">
+                            <div className="shopBannerLeft-colorBox">
+                            <i class="fa-regular fa-check"></i>
+                  </div>
+                          <div>{shopColor.color }</div>
+                </div>
+      
+                      <div className="shopBannerLeft-numberBox">{ shopColor.count}</div>
+              </div>
+              ))}
+            </div>
+
+                    
+        {/* <div className="shopBannerLeft-container-content-color">
           <div className="flex">
                       <div className="shopBannerLeft-colorBox">
                       <i class="fa-regular fa-check"></i>
@@ -15,7 +63,7 @@ export const ShopLeftFilterByColor = () => {
 
           <div className="shopBannerLeft-numberBox">12</div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
