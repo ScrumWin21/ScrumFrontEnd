81251b710b9b44e205a1a531dc4e3e759f2b41c5
Aleksandar Todorovic <90002225+aaleksandart@users.noreply.github.com>
2022-05-23 23:34:58 +0200

    RegisterForm
    Added all elements in RegisterForm component.

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -4,8 +4,36 @@ const RegisterForm = () => {
   return (
     <>
     
-        <div>
-            
+        <div className='ContainerRegular'>
+            <div className='FormSection'>
+                <form action="">
+                    <h1>Register</h1>
+                    <p>Registering for this site allows you to access your order status and history.<br/> 
+                        Just fill in the fields below, and we’ll get a new account set up for you in no<br/> 
+                        time. We will only ask you for information necessary to make the purchase<br/> 
+                        process faster and easier.</p>
+                    <div className='label-input'>
+                        <label htmlFor="">Username</label>
+                        <span>*</span>
+                        <input type="text" />
+                    </div>
+                    <div className='label-input'>
+                        <label htmlFor="">Email address</label>
+                        <span>*</span>
+                        <input type="text" />
+                    </div>
+                    <div className='label-input'>
+                        <label htmlFor="">Password</label>
+                        <span>*</span>
+                        <input type="text" />
+                    </div>
+                    <span>Your personal data will be used to support your experience<br/>
+                        throughout this website, to manage access to your account,<br/>
+                        and for other purposes described in our</span>
+                    <span>privacy policy.</span>
+                    <button>REGISTER</button>
+                </form>
+            </div>
         </div>
     
     </>
--- src/views/myAccount.js
+++ src/views/myAccount.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+//COMPONENTS
+import RegisterForm from '../components/register/RegisterForm'
+
 const myAccount = () => {
   return (
       <>
-
-        
-      
+        <RegisterForm />
       </>
   )
 }
