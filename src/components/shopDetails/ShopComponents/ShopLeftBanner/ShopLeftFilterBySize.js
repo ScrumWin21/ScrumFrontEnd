3e49810c2c2d2a658dfca57f24e7bb884fde094d
Tylerstratford <tylerstratford@hotmail.com>
2022-05-23 10:22:18 +0200

    Item #103 -  creating structure for filter by size

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterBySize.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterBySize.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 export const ShopFilterBySize = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="shopBannerLeft-title">Filter by Size</div>
+      <div className="shopBannerLeft-container">
+        <div className="shopBannerLeft-container-content-hoverAll">
+          <div>XS</div>
+          <div className="shopBannerLeft-numberBox">18</div>
+        </div>
+      </div>
+    </>
+  );
+};
