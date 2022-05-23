9986a2f5dbe39d664a64a7af9cf8740bc4ac78ed
Tylerstratford <tylerstratford@hotmail.com>
2022-05-23 12:03:23 +0200

    Item #107 - Hard code data for logo images

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByBrand.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftFilterByBrand.js
@@ -1,18 +1,50 @@
 import React from "react";
 
-import Bexim from "../../../../images/shopImages/Bexim.svg"
+import Bexim from "../../../../images/shopImages/Bexim.svg";
+import Lighton from "../../../../images/shopImages/Lighton.svg";
+import Dismis from "../../../../images/shopImages/Dismis.svg";
+import Rosimo from "../../../../images/shopImages/Rosimo.svg";
+import Dallas from "../../../../images/shopImages/Dallas.svg";
+import Concord from "../../../../images/shopImages/Concord.svg";
 
-let testGrid = ["One", "Two", "Three", "four", "five"];
+
+
+let logos = [
+  {
+    logo: Bexim,
+  },
+  {
+    logo: Lighton,
+    },
+    {
+      logo: Dismis,
+    },
+    {
+        logo: Rosimo,
+    },
+    {
+        logo: Dallas
+    },
+    {
+        logo: Concord
+    }
+];
 
 export const ShopFilterByBrand = () => {
   return (
     <>
-      <div className="shopBannerLeft-title">Filter by Brand</div>
+          <div className="shopBannerLeft-title">Filter by Brand</div>
+          
+          
+
+          
       <div className="shopBannerLeft-container">
         <div className="shopBannerLeft-filterByColorGrid">
-          {testGrid.map((testGrid) => (
-            <div className="shopBannerLeft-filterByColorGridItem">
-              {testGrid}
+          {logos.map((logos) => (
+            <div
+              className="shopBannerLeft-filterByColorGridItem"
+            >
+              <img src={logos.logo} ></img>
             </div>
           ))}
         </div>
