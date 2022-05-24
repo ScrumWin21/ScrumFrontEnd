e39f3af285417264ea2c162ef3e12cf5af8d5c0c
Tylerstratford <tylerstratford@hotmail.com>
2022-05-24 09:38:59 +0200

    Item #114 - hard coded top rated products database

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
@@ -5,60 +5,48 @@ import ProductShopTShirt from "../../../../images/shopImages/ProductShopTShirt.s
 import ProductShopShoes from "../../../../images/shopImages/ProductShopShoes.svg";
 import ProductShopSweater from "../../../../images/shopImages/ProductShopSweater.svg";
 
+let topRatedProducts = [
+  {
+    text: "Circle pattern girls...",
+    image: ProductShopTShirt,
+    price: "$190",
+  },
+  {
+    text: "Circle pattern girls...",
+    image: ProductShopShoes,
+    price: "$190",
+  },
+  {
+    text: "Circle pattern girls...",
+    image: ProductShopSweater,
+    price: "$190",
+  },
+];
+
 export const ShopLeftTopRatedProducts = () => {
   return (
     <>
       <div className="shopBannerLeft-title">Top Rated Products</div>
       <div className="shopBannerLeft-container">
         <div className="shopLeft-topRatedProducts-grid">
-          <div className="shopLeft-topRatedProducts-gridItem">
-            <div className="flex">
-              <div className="shopLeft-topRatedProducts-grid-productPicture">
-                <img src={ProductShopTShirt}></img>
-              </div>
-              <div>
-                <div className="topRatedProducts-header">
-                  Circle pattern girls...
+          {topRatedProducts.map((topRatedProducts) => (
+            <div className="shopLeft-topRatedProducts-gridItem">
+              <div className="flex">
+                <div className="shopLeft-topRatedProducts-grid-productPicture">
+                  <img src={ProductShopTShirt}></img>
                 </div>
-                <div className="topRatedProducts-stars">
-                  <img src={FiveStars}></img>
-                </div>
-                <div className="topRatedProducts-price">$190</div>
-              </div>
-            </div>
+                <div>
+                  <div className="topRatedProducts-header">
+                    Circle pattern girls...
                   </div>
-                  <div className="shopLeft-topRatedProducts-gridItem">
-            <div className="flex">
-              <div className="shopLeft-topRatedProducts-grid-productPicture">
-                <img src={ProductShopShoes}></img>
-              </div>
-              <div>
-                <div className="topRatedProducts-header">
-                  Circle pattern girls...
-                </div>
-                <div className="topRatedProducts-stars">
-                  <img src={FiveStars}></img>
-                </div>
-                <div className="topRatedProducts-price">$190</div>
-              </div>
-            </div>
+                  <div className="topRatedProducts-stars">
+                    <img src={FiveStars}></img>
                   </div>
-                  <div className="shopLeft-topRatedProducts-gridItem">
-            <div className="flex">
-              <div className="shopLeft-topRatedProducts-grid-productPicture">
-                <img src={ProductShopSweater}></img>
-              </div>
-              <div>
-                <div className="topRatedProducts-header">
-                  Circle pattern girls...
+                  <div className="topRatedProducts-price">$190</div>
                 </div>
-                <div className="topRatedProducts-stars">
-                  <img src={FiveStars}></img>
-                </div>
-                <div className="topRatedProducts-price">$190</div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
