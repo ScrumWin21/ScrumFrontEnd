921a83cfcadf5f96c029ea261ec54ffd3a3dfceb
Tylerstratford <tylerstratford@hotmail.com>
2022-05-24 08:21:10 +0200

    Item #110 basic structure created

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
@@ -1,9 +1,34 @@
 import React from "react";
 
+import FiveStars from "../../../../images/shopImages/FiveStars.svg";
+import ProductShopTShirt from "../../../../images/shopImages/ProductShopTShirt.svg";
+import ProductShopShoes from "../../../../images/shopImages/ProductShopShoes.svg";
+import ProductShopSweater from "../../../../images/shopImages/ProductShopSweater.svg";
+
 export const ShopLeftTopRatedProducts = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <div className="shopBannerLeft-title">Top Rated Products</div>
+      <div className="shopBannerLeft-container">
+        <div className="shopLeft-topRatedProducts-grid">
+          <div className="shopLeft-topRatedProducts-gridItem">
+            <div className="flex">
+              <div className="shopLeft-topRatedProducts-grid-productPicture">
+                <img src={ProductShopTShirt}></img>
+              </div>
+              <div>
+                <div className="topRatedProducts-header">
+                  Circle pattern girls...
+                </div>
+                <div className="topRatedProducts-stars">
+                  <img src={FiveStars}></img>
+                </div>
+                <div className="topRatedProducts-price">$190</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
