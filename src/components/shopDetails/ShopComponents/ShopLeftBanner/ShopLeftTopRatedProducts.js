464f4ee67d7a3576ab68ebdd4dbc5ae181791138
Tylerstratford <tylerstratford@hotmail.com>
2022-05-24 08:25:56 +0200

    Item #112 create product card and Item #113 creating rest of design -  completed

--- src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
+++ src/components/shopDetails/ShopComponents/ShopLeftBanner/ShopLeftTopRatedProducts.js
@@ -26,6 +26,38 @@ export const ShopLeftTopRatedProducts = () => {
                 <div className="topRatedProducts-price">$190</div>
               </div>
             </div>
+                  </div>
+                  <div className="shopLeft-topRatedProducts-gridItem">
+            <div className="flex">
+              <div className="shopLeft-topRatedProducts-grid-productPicture">
+                <img src={ProductShopShoes}></img>
+              </div>
+              <div>
+                <div className="topRatedProducts-header">
+                  Circle pattern girls...
+                </div>
+                <div className="topRatedProducts-stars">
+                  <img src={FiveStars}></img>
+                </div>
+                <div className="topRatedProducts-price">$190</div>
+              </div>
+            </div>
+                  </div>
+                  <div className="shopLeft-topRatedProducts-gridItem">
+            <div className="flex">
+              <div className="shopLeft-topRatedProducts-grid-productPicture">
+                <img src={ProductShopSweater}></img>
+              </div>
+              <div>
+                <div className="topRatedProducts-header">
+                  Circle pattern girls...
+                </div>
+                <div className="topRatedProducts-stars">
+                  <img src={FiveStars}></img>
+                </div>
+                <div className="topRatedProducts-price">$190</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
