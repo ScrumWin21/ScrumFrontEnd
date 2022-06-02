ad9a8ae1f454144baae5663021fefa85d813117a
Jonke-ec <jonas.askling@utb.ecutbildning.se>
2022-06-02 13:03:43 +0200

    Views folder created and productpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-
+import { Products } from "./components/shopDetails/Products";
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
